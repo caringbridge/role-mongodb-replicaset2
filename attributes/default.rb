403c5a0955f043018804e3b9d36f67f7c9c35140
svanzee4cb <svanzee@Van-Zees-MacBook-Pro.local>
2014-12-09 16:03:32 -0600

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['mongodb']['config']['replSet'] = 'rs_shard2'
+default['mongodb']['cluster_name'] = 'vzdev'
+default['mongodb']['is_replicaset'] = true
+default['mongodb']['shard_name'] = 'shard2'
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'role-mongodb-replicaset2'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures role-mongodb-replicaset2'
+long_description 'Installs/Configures role-mongodb-replicaset2'
+version          '0.1.0'
+
+depends 'mongodb', '0.16.1'
+depends 'yum'
